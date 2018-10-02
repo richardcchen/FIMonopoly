56faef21895a189f50772556195a5fd2042390a1
github username <github email address>
2018-10-01 22:37:36 -0400

    Successfully Add Events to backend

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,323 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+events = [{
+      "board_id": 1,
+      "name": "GO",
+      "description": "Game Starts",
+      "event_type": "go",
+      "energy": -1000,
+      "IQ": 0
+    },
+    {
+      "board_id": 2,
+      "name": "Prework",
+      "description": "Finished Prework",
+      "event_type": "purple",
+      "energy": 60,
+      "IQ": 2
+    },
+    {
+      "board_id": 3,
+      "name": "Instructor requests for a talk",
+      "description": "Got feedback for how to study",
+      "event_type": "chest",
+      "energy": -1000,
+      "IQ": 0
+    },
+    {
+      "board_id": 4,
+      "name": "Intro to OO",
+      "description": "Learned Object Orientation",
+      "event_type": "purple",
+      "energy": 60,
+      "IQ": 4
+    },
+    {
+      "board_id": 5,
+      "name": "You became sick",
+      "description": "Take a Rest at Home",
+      "event_type": "income tax",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 6,
+      "name": "Jonathan 1 on 1",
+      "description": "Got advices from Jon",
+      "event_type": "railroad",
+      "energy": -200,
+      "IQ": 0
+    },
+    {
+      "board_id": 7,
+      "name": "Ruby One to Many",
+      "description": "Learned One to Many Relationship",
+      "event_type": "light blue",
+      "energy": 100,
+      "IQ": 6
+    },
+    {
+      "board_id": 8,
+      "name": "Pairing Lab",
+      "description": "Paired with classmate and Learned more",
+      "event_type": "chance",
+      "energy": 100,
+      "IQ": 2
+    },
+    {
+      "board_id": 9,
+      "name": "Ruby Many to Many",
+      "description": "Learned Many to Many Relationship",
+      "event_type": "light blue",
+      "energy": 100,
+      "IQ": 6
+    },
+    {
+      "board_id": 10,
+      "name": "ActiveRecord",
+      "description": "Learned ActiveRecord",
+      "event_type": "light blue",
+      "energy": 120,
+      "IQ": 8
+    },
+    {
+      "board_id": 11,
+      "name": "Study for Code Challenge at Home",
+      "description": "Review all material in within the Mod",
+      "event_type": "jail",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 12,
+      "name": "Intro to Sinatra",
+      "description": "Learned the Basis of Sinatra",
+      "event_type": "pink",
+      "energy": 130,
+      "IQ": 10
+    },
+    {
+      "board_id": 13,
+      "name": "SQL",
+      "description": "Get more Energy",
+      "event_type": "electric company",
+      "energy": 150,
+      "IQ": 0
+    },
+    {
+      "board_id": 14,
+      "name": "Sinatra Forms",
+      "description": "Learned Sinatra Forms",
+      "event_type": "pink",
+      "energy": 140,
+      "IQ": 10
+    },
+    {
+      "board_id": 15,
+      "name": "Sinatra Full CRUD",
+      "description": "Learn Sinatra Full CRUD",
+      "event_type": "pink",
+      "energy": 160,
+      "IQ": 12
+    },
+    {
+      "board_id": 16,
+      "name": "Garry 1 on 1",
+      "description": "Got advices from Garry",
+      "event_type": "railroad",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 17,
+      "name": "Intro to Rails",
+      "description": "Learn the Basis of Rails",
+      "event_type": "orange",
+      "energy": 160,
+      "IQ": 14
+    },
+    {
+      "board_id": 18,
+      "name": "Instructor requests for a talk",
+      "description": "Got feedback for how to study",
+      "event_type": "chest",
+      "energy": -1000,
+      "IQ": 0
+    },
+    {
+      "board_id": 19,
+      "name": "Rails Forms && Valboard_idations",
+      "description": "Learned more on Rails",
+      "event_type": "orange",
+      "energy": 160,
+      "IQ": 14
+    },
+    {
+      "board_id": 20,
+      "name": "RESTful Routes",
+      "description": "Learned RESTful Routes ",
+      "event_type": "orange",
+      "energy": 200,
+      "IQ": 16
+    },
+    {
+      "board_id": 21,
+      "name": "Breaktime",
+      "description": "Take a break",
+      "event_type": "free parking",
+      "energy": 0,
+      "IQ": 0
+    },
+    {
+      "board_id": 22,
+      "name": "Intro to JS",
+      "description": "Learned the Basis of JavaScript",
+      "event_type": "red",
+      "energy": 220,
+      "IQ": 18
+    },
+    {
+      "board_id": 23,
+      "name": "Pairing Lab",
+      "description": "Paired with classmate and Learned more",
+      "event_type": "chance",
+      "energy": 100,
+      "IQ": 2
+    },
+    {
+      "board_id": 24,
+      "name": "JS Functional Programming",
+      "description": "Learned more on JavaScript",
+      "event_type": "red",
+      "energy": 220,
+      "IQ": 18
+    },
+    {
+      "board_id": 25,
+      "name": "JS Object Oriented",
+      "description": "Learned JavaScript Object Orientation",
+      "event_type": "red",
+      "energy": 240,
+      "IQ": 20
+    },
+    {
+      "board_id": 26,
+      "name": "Watching Evans's Costumes",
+      "description": "Have Fun and Relaxed",
+      "event_type": "railroad",
+      "energy": -200,
+      "IQ": 0
+    },
+    {
+      "board_id": 27,
+      "name": "Intro to DOM",
+      "description": "Leaned Document Object Model",
+      "event_type": "yellow",
+      "energy": 260,
+      "IQ": 0
+    },
+    {
+      "board_id": 28,
+      "name": "Learning Fetch",
+      "description": "Learned Fetch Requests",
+      "event_type": "yellow",
+      "energy": 260,
+      "IQ": 22
+    },
+    {
+      "board_id": 29,
+      "name": "Flatiron Student Present",
+      "description": "Finished FSP",
+      "event_type": "water",
+      "energy": 150,
+      "IQ": 0
+    },
+    {
+      "board_id": 30,
+      "name": "JS Using Rails API",
+      "description": "Leaned how to build API",
+      "event_type": "yellow",
+      "energy": 280,
+      "IQ": 24
+    },
+    {
+      "board_id": 31,
+      "name": "Failed Code Challenge",
+      "description": "Failed and Being Sad",
+      "event_type": "go to jail",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 32,
+      "name": "Intro to React",
+      "description": "Learned the Basis of React",
+      "event_type": "green",
+      "energy": 300,
+      "IQ": 26
+    },
+    {
+      "board_id": 33,
+      "name": "JSX and Props",
+      "description": "Learned JSX and Props",
+      "event_type": "green",
+      "energy": 300,
+      "IQ": 26
+    },
+    {
+      "board_id": 34,
+      "name": "Instructor requests for a talk",
+      "description": "Got feedback for how to study",
+      "event_type": "chest",
+      "energy": -1000,
+      "IQ": 0
+    },
+    {
+      "board_id": 35,
+      "name": "Props and State",
+      "description": "Learned Props and State",
+      "event_type": "green",
+      "energy": 320,
+      "IQ": 28
+    },
+    {
+      "board_id": 36,
+      "name": "Andrew 1 on 1",
+      "description": "Got advices from Andrew",
+      "event_type": "railroad",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 37,
+      "name": "Pairing Lab",
+      "description": "Paired with classmate and Learned more",
+      "event_type": "chance",
+      "energy": 100,
+      "IQ": 2
+    },
+    {
+      "board_id": 38,
+      "name": "Lifecycle in React",
+      "description": "Learned Lifecycle in React",
+      "event_type": "blue",
+      "energy": 350,
+      "IQ": 35
+    },
+    {
+      "board_id": 39,
+      "name": "Your Final Project Topic was Rejected",
+      "description": "Need Additional Effort to find Another Topic",
+      "event_type": "blue",
+      "energy": 100,
+      "IQ": 0
+    },
+    {
+      "board_id": 40,
+      "name": "Learning Redux",
+      "description": "Learned Redux",
+      "event_type": "blue",
+      "energy": 400,
+      "IQ": 50
+    }
+  ]
+
+events.each {|event| Event.create(event)}
