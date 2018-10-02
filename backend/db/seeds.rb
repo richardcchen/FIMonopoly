50d5ecc574c89e6d9792a2e35a17a030c764a8c7
Richard Chen <richard.c.chen@gmail.com>
2018-10-02 14:54:11 -0400

    added board

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,22 +1,7 @@
-<<<<<<< HEAD
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-{
-  "events": [{
-      "board_id": 1,
-      "name": "GO",
-      "description": ,
-=======
 events = [{
       "board_id": 1,
       "name": "GO",
       "description": "Game Starts",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "go",
       "energy": -1000,
       "IQ": 0
@@ -24,11 +9,7 @@ events = [{
     {
       "board_id": 2,
       "name": "Prework",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Finished Prework",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "purple",
       "energy": 60,
       "IQ": 2
@@ -36,11 +17,7 @@ events = [{
     {
       "board_id": 3,
       "name": "Instructor requests for a talk",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Got feedback for how to study",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "chest",
       "energy": -1000,
       "IQ": 0
@@ -48,11 +25,7 @@ events = [{
     {
       "board_id": 4,
       "name": "Intro to OO",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned Object Orientation",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "purple",
       "energy": 60,
       "IQ": 4
@@ -60,11 +33,7 @@ events = [{
     {
       "board_id": 5,
       "name": "You became sick",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Take a Rest at Home",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "income tax",
       "energy": 200,
       "IQ": 0
@@ -72,11 +41,7 @@ events = [{
     {
       "board_id": 6,
       "name": "Jonathan 1 on 1",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Got advices from Jon",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "railroad",
       "energy": -200,
       "IQ": 0
@@ -84,11 +49,7 @@ events = [{
     {
       "board_id": 7,
       "name": "Ruby One to Many",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned One to Many Relationship",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "light blue",
       "energy": 100,
       "IQ": 6
@@ -96,11 +57,7 @@ events = [{
     {
       "board_id": 8,
       "name": "Pairing Lab",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Paired with classmate and Learned more",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "chance",
       "energy": 100,
       "IQ": 2
@@ -108,11 +65,7 @@ events = [{
     {
       "board_id": 9,
       "name": "Ruby Many to Many",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned Many to Many Relationship",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "light blue",
       "energy": 100,
       "IQ": 6
@@ -120,11 +73,7 @@ events = [{
     {
       "board_id": 10,
       "name": "ActiveRecord",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned ActiveRecord",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "light blue",
       "energy": 120,
       "IQ": 8
@@ -132,11 +81,7 @@ events = [{
     {
       "board_id": 11,
       "name": "Study for Code Challenge at Home",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Review all material in within the Mod",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "jail",
       "energy": 200,
       "IQ": 0
@@ -144,11 +89,7 @@ events = [{
     {
       "board_id": 12,
       "name": "Intro to Sinatra",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned the Basis of Sinatra",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "pink",
       "energy": 130,
       "IQ": 10
@@ -156,11 +97,7 @@ events = [{
     {
       "board_id": 13,
       "name": "SQL",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Get more Energy",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "electric company",
       "energy": 150,
       "IQ": 0
@@ -168,11 +105,7 @@ events = [{
     {
       "board_id": 14,
       "name": "Sinatra Forms",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned Sinatra Forms",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "pink",
       "energy": 140,
       "IQ": 10
@@ -180,11 +113,7 @@ events = [{
     {
       "board_id": 15,
       "name": "Sinatra Full CRUD",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learn Sinatra Full CRUD",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "pink",
       "energy": 160,
       "IQ": 12
@@ -192,11 +121,7 @@ events = [{
     {
       "board_id": 16,
       "name": "Garry 1 on 1",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Got advices from Garry",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "railroad",
       "energy": 200,
       "IQ": 0
@@ -204,11 +129,7 @@ events = [{
     {
       "board_id": 17,
       "name": "Intro to Rails",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learn the Basis of Rails",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "orange",
       "energy": 160,
       "IQ": 14
@@ -216,11 +137,7 @@ events = [{
     {
       "board_id": 18,
       "name": "Instructor requests for a talk",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Got feedback for how to study",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "chest",
       "energy": -1000,
       "IQ": 0
@@ -228,24 +145,15 @@ events = [{
     {
       "board_id": 19,
       "name": "Rails Forms && Valboard_idations",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned more on Rails",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "orange",
       "energy": 160,
       "IQ": 14
     },
     {
       "board_id": 20,
-<<<<<<< HEAD
-      "name": "Restful Routes",
-      "description": ,
-=======
       "name": "RESTful Routes",
       "description": "Learned RESTful Routes ",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "orange",
       "energy": 200,
       "IQ": 16
@@ -253,11 +161,7 @@ events = [{
     {
       "board_id": 21,
       "name": "Breaktime",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Take a break",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "free parking",
       "energy": 0,
       "IQ": 0
@@ -265,11 +169,7 @@ events = [{
     {
       "board_id": 22,
       "name": "Intro to JS",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned the Basis of JavaScript",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "red",
       "energy": 220,
       "IQ": 18
@@ -277,11 +177,7 @@ events = [{
     {
       "board_id": 23,
       "name": "Pairing Lab",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Paired with classmate and Learned more",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "chance",
       "energy": 100,
       "IQ": 2
@@ -289,11 +185,7 @@ events = [{
     {
       "board_id": 24,
       "name": "JS Functional Programming",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned more on JavaScript",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "red",
       "energy": 220,
       "IQ": 18
@@ -301,11 +193,7 @@ events = [{
     {
       "board_id": 25,
       "name": "JS Object Oriented",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned JavaScript Object Orientation",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "red",
       "energy": 240,
       "IQ": 20
@@ -313,25 +201,15 @@ events = [{
     {
       "board_id": 26,
       "name": "Watching Evans's Costumes",
-<<<<<<< HEAD
-      "description": ,
-      "event_type": "railroad",
-      "energy": 200,
-=======
       "description": "Have Fun and Relaxed",
       "event_type": "railroad",
       "energy": -200,
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "IQ": 0
     },
     {
       "board_id": 27,
       "name": "Intro to DOM",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Leaned Document Object Model",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "yellow",
       "energy": 260,
       "IQ": 0
@@ -339,11 +217,7 @@ events = [{
     {
       "board_id": 28,
       "name": "Learning Fetch",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned Fetch Requests",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "yellow",
       "energy": 260,
       "IQ": 22
@@ -351,11 +225,7 @@ events = [{
     {
       "board_id": 29,
       "name": "Flatiron Student Present",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Finished FSP",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "water",
       "energy": 150,
       "IQ": 0
@@ -363,11 +233,7 @@ events = [{
     {
       "board_id": 30,
       "name": "JS Using Rails API",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Leaned how to build API",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "yellow",
       "energy": 280,
       "IQ": 24
@@ -375,25 +241,15 @@ events = [{
     {
       "board_id": 31,
       "name": "Failed Code Challenge",
-<<<<<<< HEAD
-      "description": ,
-      "event_type": "go to jail",
-      "energy": -1000,
-=======
       "description": "Failed and Being Sad",
       "event_type": "go to jail",
       "energy": 200,
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "IQ": 0
     },
     {
       "board_id": 32,
       "name": "Intro to React",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned the Basis of React",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "green",
       "energy": 300,
       "IQ": 26
@@ -401,11 +257,7 @@ events = [{
     {
       "board_id": 33,
       "name": "JSX and Props",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned JSX and Props",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "green",
       "energy": 300,
       "IQ": 26
@@ -413,11 +265,7 @@ events = [{
     {
       "board_id": 34,
       "name": "Instructor requests for a talk",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Got feedback for how to study",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "chest",
       "energy": -1000,
       "IQ": 0
@@ -425,11 +273,7 @@ events = [{
     {
       "board_id": 35,
       "name": "Props and State",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned Props and State",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "green",
       "energy": 320,
       "IQ": 28
@@ -437,11 +281,7 @@ events = [{
     {
       "board_id": 36,
       "name": "Andrew 1 on 1",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Got advices from Andrew",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "railroad",
       "energy": 200,
       "IQ": 0
@@ -449,11 +289,7 @@ events = [{
     {
       "board_id": 37,
       "name": "Pairing Lab",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Paired with classmate and Learned more",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "chance",
       "energy": 100,
       "IQ": 2
@@ -461,11 +297,7 @@ events = [{
     {
       "board_id": 38,
       "name": "Lifecycle in React",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned Lifecycle in React",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "blue",
       "energy": 350,
       "IQ": 35
@@ -473,33 +305,18 @@ events = [{
     {
       "board_id": 39,
       "name": "Your Final Project Topic was Rejected",
-<<<<<<< HEAD
-      "description": ,
-      "event_type": "blue",
-      "energy": -75,
-=======
       "description": "Need Additional Effort to find Another Topic",
       "event_type": "blue",
       "energy": 100,
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "IQ": 0
     },
     {
       "board_id": 40,
       "name": "Learning Redux",
-<<<<<<< HEAD
-      "description": ,
-=======
       "description": "Learned Redux",
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
       "event_type": "blue",
       "energy": 400,
       "IQ": 50
     }
   ]
-<<<<<<< HEAD
-}
-=======
-
-events.each {|event| Event.create(board_id: event.board_id, name: event.name, description: event.description, event_type: event.event_type, energy: event.energy, IQ: event.IQ)}
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
+events.each {|event| Event.create(event)}
--- src/index.js
+++ src/index.js
@@ -1,127 +1,39 @@
-let canvas
-let ctx
-let x
-let y
-let dx
-let dy
-let up
 document.addEventListener("DOMContentLoaded", function() {
-<<<<<<< HEAD
-  canvas = document.getElementById("myCanvas")
-  ctx = canvas.getContext("2d")
-
-  //ctx.fillStyle = "grey"
-  //ctx.fillRect(0, 0, 1000, 1000)
-  x = 50;
-  y = 250;
-  dx = 2;
-  dy = 2;
-  up
-  let upButton = document.getElementById('up')
-
-  make_base()
-
-
-=======
   store = []
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
-  const diceDisplay = document.getElementById('diceContainer')
-  diceDisplay.addEventListener("click", event => {
-    if (event.target.id === "rollDice") {
-      AniDice()
-    }
-    if (event.target.id === "stopRoll") {
-      stopDice()
-    }
-
-  }) //end of dice roll listener
-
-<<<<<<< HEAD
-  //Using Buttons to move Ball
-  upButton.addEventListener("click", event => {
-    let distance = y - 200
-    up = setInterval(() => {
-      moveUp(distance)
-    }, 10);
-
-    console.log(up);
-  })
-
-
-  function moveUp(distance) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    if (y < distance || y < 0) {
-      //debugger
-      clearInterval(up)
-    }
-    y -= dy;
-  }
-
-=======
+  cornerArray = [1, 11, 21, 31]
+  specialArray = [3, 5, 6, 8, 13, 16, 18, 23, 26, 29, 34, 36, 37, 39]
+  bottomRow = [2,4,7,9,10]
+  leftRow = [12,14,15,17,19,20]
+  topRow = [22, 24, 25, 27, 28, 30]
+  rightRow = [32, 33, 35, 38, 40]
   fetch('http://localhost:3000/api/v1/events')
-  .then(response => response.json())
-  .then(eventData => {
-    store = [...eventData];
-    
-  })
->>>>>>> 745aa6874ef9dd6420ba530f0ac7785a52219500
+    .then(response => response.json())
+    .then(eventData => {
+      store = [...eventData];
+      store.forEach(event => {
+        if (cornerArray.includes(+event.board_id)) {}
+        else if (specialArray.includes(+event.board_id)) {}
+        else if (bottomRow.includes(+event.board_id)) {
+          const eventId = document.getElementById(`${event.board_id}`)
+          eventId.children[1].innerText = event.name
+          eventId.children[2].innerText = `Energy: ${event.energy}, IQ: ${event.IQ}`
+        }else if (leftRow.includes(+event.board_id)) {
+          const eventId = document.getElementById(`${event.board_id}`)
+          eventId.children[0].children[0].innerText = event.name
+          eventId.children[0].children[1].innerText = `Energy: ${event.energy}, IQ: ${event.IQ}`
+        } else if (topRow.includes(+event.board_id)) {
+          const eventId = document.getElementById(`${event.board_id}`)
+          eventId.children[1].innerText = event.name
+          eventId.children[2].innerText = `Energy: ${event.energy}, IQ: ${event.IQ}`
+        } else if (rightRow.includes(+event.board_id)) {
+          const eventId = document.getElementById(`${event.board_id}`)
+          eventId.children[0].children[0].innerText = event.name
+          eventId.children[0].children[1].innerText = `Energy: ${event.energy}, IQ: ${event.IQ}`
+        }
+      }) //end of for each
+    })
 
-  // store.map(event => {
-  //   document.getElementById(event.board_id){
-  //     //code for divs
-  //
-  //   }
-  // })
+
 
 
 }) // end of document listener
-
-function AniDice() {
-  MyVar = setInterval(rolldice, 200)
-}
-
-function rolldice() {
-  var ranNum = Math.floor(1 + Math.random() * 6);
-  document.getElementById("dice").innerHTML = ranNum;
-}
-
-function stopDice() {
-  clearInterval(MyVar)
-}
-
-function drawBall() {
-  ctx.beginPath();
-  //ctx.arc(x, y, 10, 0, Math.PI * 2);
-  //ctx.fillStyle = "red";
-  //ctx.fill();
-  ctx.drawImage(base_image, x, y, 50, 50)
-  ctx.closePath();
-}
-
-function moveUp(distance) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
-  console.log(y, distance);
-  //debugger
-  if (y < distance) {
-    debugger
-    clearInterval(up)
-  }
-  y -= dy;
-}
-
-function moveDown() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
-  //x += dx;
-  y -= dy;
-}
-
-function make_base() {
-  base_image = new Image();
-  base_image.src = 'https://miro.medium.com/max/2400/1*7Kog7HLU5yoLaLpCi4VcuA.png';
-  base_image.onload = function() {
-    ctx.drawImage(base_image, 50, 250, 50, 50);
-  }
-}
