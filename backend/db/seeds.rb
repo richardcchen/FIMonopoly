103d08e8aaad74ffc155d1e24bb51fe940cd5fa2
Richard Chen <richard.c.chen@gmail.com>
2018-10-02 11:26:12 -0400

    added movement features

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,326 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+{
+  "events": [{
+      "board_id": 1,
+      "name": "GO",
+      "description": ,
+      "event_type": "go",
+      "energy": -1000,
+      "IQ": 0
+    },
+    {
+      "board_id": 2,
+      "name": "Prework",
+      "description": ,
+      "event_type": "purple",
+      "energy": 60,
+      "IQ": 2
+    },
+    {
+      "board_id": 3,
+      "name": "Instructor requests for a talk",
+      "description": ,
+      "event_type": "chest",
+      "energy": -1000,
+      "IQ": 0
+    },
+    {
+      "board_id": 4,
+      "name": "Intro to OO",
+      "description": ,
+      "event_type": "purple",
+      "energy": 60,
+      "IQ": 4
+    },
+    {
+      "board_id": 5,
+      "name": "You became sick",
+      "description": ,
+      "event_type": "income tax",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 6,
+      "name": "Jonathan 1 on 1",
+      "description": ,
+      "event_type": "railroad",
+      "energy": -200,
+      "IQ": 0
+    },
+    {
+      "board_id": 7,
+      "name": "Ruby One to Many",
+      "description": ,
+      "event_type": "light blue",
+      "energy": 100,
+      "IQ": 6
+    },
+    {
+      "board_id": 8,
+      "name": "Pairing Lab",
+      "description": ,
+      "event_type": "chance",
+      "energy": 100,
+      "IQ": 2
+    },
+    {
+      "board_id": 9,
+      "name": "Ruby Many to Many",
+      "description": ,
+      "event_type": "light blue",
+      "energy": 100,
+      "IQ": 6
+    },
+    {
+      "board_id": 10,
+      "name": "ActiveRecord",
+      "description": ,
+      "event_type": "light blue",
+      "energy": 120,
+      "IQ": 8
+    },
+    {
+      "board_id": 11,
+      "name": "Study for Code Challenge at Home",
+      "description": ,
+      "event_type": "jail",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 12,
+      "name": "Intro to Sinatra",
+      "description": ,
+      "event_type": "pink",
+      "energy": 130,
+      "IQ": 10
+    },
+    {
+      "board_id": 13,
+      "name": "SQL",
+      "description": ,
+      "event_type": "electric company",
+      "energy": 150,
+      "IQ": 0
+    },
+    {
+      "board_id": 14,
+      "name": "Sinatra Forms",
+      "description": ,
+      "event_type": "pink",
+      "energy": 140,
+      "IQ": 10
+    },
+    {
+      "board_id": 15,
+      "name": "Sinatra Full CRUD",
+      "description": ,
+      "event_type": "pink",
+      "energy": 160,
+      "IQ": 12
+    },
+    {
+      "board_id": 16,
+      "name": "Garry 1 on 1",
+      "description": ,
+      "event_type": "railroad",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 17,
+      "name": "Intro to Rails",
+      "description": ,
+      "event_type": "orange",
+      "energy": 160,
+      "IQ": 14
+    },
+    {
+      "board_id": 18,
+      "name": "Instructor requests for a talk",
+      "description": ,
+      "event_type": "chest",
+      "energy": -1000,
+      "IQ": 0
+    },
+    {
+      "board_id": 19,
+      "name": "Rails Forms && Valboard_idations",
+      "description": ,
+      "event_type": "orange",
+      "energy": 160,
+      "IQ": 14
+    },
+    {
+      "board_id": 20,
+      "name": "Restful Routes",
+      "description": ,
+      "event_type": "orange",
+      "energy": 200,
+      "IQ": 16
+    },
+    {
+      "board_id": 21,
+      "name": "Breaktime",
+      "description": ,
+      "event_type": "free parking",
+      "energy": 0,
+      "IQ": 0
+    },
+    {
+      "board_id": 22,
+      "name": "Intro to JS",
+      "description": ,
+      "event_type": "red",
+      "energy": 220,
+      "IQ": 18
+    },
+    {
+      "board_id": 23,
+      "name": "Pairing Lab",
+      "description": ,
+      "event_type": "chance",
+      "energy": 100,
+      "IQ": 2
+    },
+    {
+      "board_id": 24,
+      "name": "JS Functional Programming",
+      "description": ,
+      "event_type": "red",
+      "energy": 220,
+      "IQ": 18
+    },
+    {
+      "board_id": 25,
+      "name": "JS Object Oriented",
+      "description": ,
+      "event_type": "red",
+      "energy": 240,
+      "IQ": 20
+    },
+    {
+      "board_id": 26,
+      "name": "Watching Evans's Costumes",
+      "description": ,
+      "event_type": "railroad",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 27,
+      "name": "Intro to DOM",
+      "description": ,
+      "event_type": "yellow",
+      "energy": 260,
+      "IQ": 0
+    },
+    {
+      "board_id": 28,
+      "name": "Learning Fetch",
+      "description": ,
+      "event_type": "yellow",
+      "energy": 260,
+      "IQ": 22
+    },
+    {
+      "board_id": 29,
+      "name": "Flatiron Student Present",
+      "description": ,
+      "event_type": "water",
+      "energy": 150,
+      "IQ": 0
+    },
+    {
+      "board_id": 30,
+      "name": "JS Using Rails API",
+      "description": ,
+      "event_type": "yellow",
+      "energy": 280,
+      "IQ": 24
+    },
+    {
+      "board_id": 31,
+      "name": "Failed Code Challenge",
+      "description": ,
+      "event_type": "go to jail",
+      "energy": -1000,
+      "IQ": 0
+    },
+    {
+      "board_id": 32,
+      "name": "Intro to React",
+      "description": ,
+      "event_type": "green",
+      "energy": 300,
+      "IQ": 26
+    },
+    {
+      "board_id": 33,
+      "name": "JSX and Props",
+      "description": ,
+      "event_type": "green",
+      "energy": 300,
+      "IQ": 26
+    },
+    {
+      "board_id": 34,
+      "name": "Instructor requests for a talk",
+      "description": ,
+      "event_type": "chest",
+      "energy": -1000,
+      "IQ": 0
+    },
+    {
+      "board_id": 35,
+      "name": "Props and State",
+      "description": ,
+      "event_type": "green",
+      "energy": 320,
+      "IQ": 28
+    },
+    {
+      "board_id": 36,
+      "name": "Andrew 1 on 1",
+      "description": ,
+      "event_type": "railroad",
+      "energy": 200,
+      "IQ": 0
+    },
+    {
+      "board_id": 37,
+      "name": "Pairing Lab",
+      "description": ,
+      "event_type": "chance",
+      "energy": 100,
+      "IQ": 2
+    },
+    {
+      "board_id": 38,
+      "name": "Lifecycle in React",
+      "description": ,
+      "event_type": "blue",
+      "energy": 350,
+      "IQ": 35
+    },
+    {
+      "board_id": 39,
+      "name": "Your Final Project Topic was Rejected",
+      "description": ,
+      "event_type": "blue",
+      "energy": -75,
+      "IQ": 0
+    },
+    {
+      "board_id": 40,
+      "name": "Learning Redux",
+      "description": ,
+      "event_type": "blue",
+      "energy": 400,
+      "IQ": 50
+    }
+  ]
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,58 @@
+let canvas
+let ctx
+let x
+let y
+let dx
+let dy
+let up
 document.addEventListener("DOMContentLoaded", function() {
+  canvas = document.getElementById("myCanvas")
+  ctx = canvas.getContext("2d")
+
+  //ctx.fillStyle = "grey"
+  //ctx.fillRect(0, 0, 1000, 1000)
+  x = 50;
+  y = 250;
+  dx = 2;
+  dy = 2;
+  up
+  let upButton = document.getElementById('up')
+
+  make_base()
+
 
   const diceDisplay = document.getElementById('diceContainer')
-  diceDisplay.addEventListener("click", event =>{
-    if (event.target.id === "rollDice"){
+  diceDisplay.addEventListener("click", event => {
+    if (event.target.id === "rollDice") {
       AniDice()
     }
-    if (event.target.id === "stopRoll"){
+    if (event.target.id === "stopRoll") {
       stopDice()
     }
 
   }) //end of dice roll listener
+
+  //Using Buttons to move Ball
+  upButton.addEventListener("click", event => {
+    let distance = y - 200
+    up = setInterval(() => {
+      moveUp(distance)
+    }, 10);
+
+    console.log(up);
+  })
+
+
+  function moveUp(distance) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    if (y < distance || y < 0) {
+      //debugger
+      clearInterval(up)
+    }
+    y -= dy;
+  }
+
 
 
 }) // end of document listener
@@ -18,11 +61,47 @@ function AniDice() {
   MyVar = setInterval(rolldice, 200)
 }
 
-function rolldice(){
+function rolldice() {
   var ranNum = Math.floor(1 + Math.random() * 6);
   document.getElementById("dice").innerHTML = ranNum;
 }
 
-function stopDice(){
+function stopDice() {
   clearInterval(MyVar)
+}
+
+function drawBall() {
+  ctx.beginPath();
+  //ctx.arc(x, y, 10, 0, Math.PI * 2);
+  //ctx.fillStyle = "red";
+  //ctx.fill();
+  ctx.drawImage(base_image, x, y, 50, 50)
+  ctx.closePath();
+}
+
+function moveUp(distance) {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  console.log(y, distance);
+  //debugger
+  if (y < distance) {
+    debugger
+    clearInterval(up)
+  }
+  y -= dy;
+}
+
+function moveDown() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  //x += dx;
+  y -= dy;
+}
+
+function make_base() {
+  base_image = new Image();
+  base_image.src = 'https://miro.medium.com/max/2400/1*7Kog7HLU5yoLaLpCi4VcuA.png';
+  base_image.onload = function() {
+    ctx.drawImage(base_image, 50, 250, 50, 50);
+  }
 }
